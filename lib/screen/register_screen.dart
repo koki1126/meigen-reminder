f9319fc64acc8cb50dd6a6815fcbc8466376bc05
koki <deguguti@yahoo.co.jp>
2022-06-28 21:44:17 +0900

    一括

--- lib/screen/register_screen.dart
+++ lib/screen/register_screen.dart
@@ -1,16 +1,39 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
-class RegisterScreen extends StatelessWidget {
+class RegisterScreen extends StatefulWidget {
   const RegisterScreen({Key? key}) : super(key: key);
 
   @override
+  State<RegisterScreen> createState() => _RegisterScreenState();
+}
+
+class _RegisterScreenState extends State<RegisterScreen> {
+  @override
   Widget build(BuildContext context) {
-    return const Scaffold(
+    return Scaffold(
       backgroundColor: Colors.pink,
       body: Center(
-        child: Text(
-          'This is register_screen',
-          style: TextStyle(color: Colors.white),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            const Text(
+              'テスト',
+              style: TextStyle(
+                fontSize: 40,
+              ),
+            ),
+            ElevatedButton(
+              onPressed: () {
+                setState(
+                  () {
+                    Navigator.pop(context);
+                  },
+                );
+              },
+              child: Text('登録する'),
+            ),
+          ],
         ),
       ),
     );
